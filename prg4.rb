325fc149709920d52089d23e41d14da4acc2e401
SOWMYA0604 <85829094+SOWMYA0604@users.noreply.github.com>
2023-03-15 20:06:05 +0530

    Create prg4.rb

---
+++ prg4.rb
@@ -0,0 +1,15 @@
+#file = "#file = "/home/it/Downloads/12b4/labprg4.rb"
+# file name
+fbname = File.basename "/home/it/Downloads/12b4/labprg4.rb"
+puts "File name: "+fbname
+
+# basename
+bname = File.basename "/home/it/Downloads/12b4/labprg4.rb",".rb"
+puts "Base name: "+bname
+
+# file extention
+ffextn = File.extname  "/home/it/Downloads/12b4/labprg4.rb"
+puts "Extention: "+ffextn
+# path name
+path_name= File.dirname  "/home/it/Downloads/12b4/labprg4.rb"
+puts "Path name: "+path_name"
