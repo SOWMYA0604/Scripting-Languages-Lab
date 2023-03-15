dee2f30206be58eefbf87482a8ae1518bcb49b32
SOWMYA0604 <85829094+SOWMYA0604@users.noreply.github.com>
2023-03-15 20:08:34 +0530

    Create prg7.rb

---
+++ prg7.rb
@@ -0,0 +1,12 @@
+def check(x,y)
+if (x==20 || y==20)
+return true;
+    else
+    return x+y;
+    end
+end
+print check(10, 10),"\n"
+print check(40, 10),"\n"
+print check(15, 20),"\n"
+print check(20, 20),"\n"
+print check(20, 10),"\n"
